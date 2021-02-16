59aaef212864019656cd710d6fa17bfa59be737a
TangJian <905286484@qq.com>
2021-02-16 18:05:45 +0800

    perf: 优化日历js逻辑

--- miniprogram/pages/calendar/index.js
+++ miniprogram/pages/calendar/index.js
@@ -48,15 +48,13 @@ Page({
     console.log("afterTapDate", e.detail); // => { year: 2019, month: 12, date: 3, ...}
     const { year, month, date, todoText } = e.detail;
     const todayPunch = [];
-    if (todoText) {
-      for (const item of todoText) {
-        const punchGoal = this.data.punchGoalList.find(
-          (punchGoalItem) => punchGoalItem._id === item.punchGoalId
-        );
-        punchGoal.comment = item.comment;
-        punchGoal.punchId = item._id;
-        todayPunch.push(punchGoal);
-      }
+    for (const item of todoText) {
+      const punchGoal = this.data.punchGoalList.find(
+        (punchGoalItem) => punchGoalItem._id === item.punchGoalId
+      );
+      punchGoal.comment = item.comment;
+      punchGoal.punchId = item._id;
+      todayPunch.push(punchGoal);
     }
     this.setData({ selectDate: { year, month, date }, todayPunch });
   },
@@ -65,11 +63,9 @@ Page({
     console.log("whenChangeMonth", e.detail); // => { current: { month: 3, ... }, next: { month: 4, ... }}
     const { year, month } = e.detail.next;
     this.data.currentMonth = { year, month };
-    if (!this.data.punchList[`punchList-${year}-${month}`]) {
-      this.getData(year, month);
-    } else {
-      this.setTodos(this.data.punchList[`punchList-${year}-${month}`]);
-    }
+    this.data.punchList[`punchList-${year}-${month}`]
+      ? this.setTodos(this.data.punchList[`punchList-${year}-${month}`])
+      : this.getData(year, month);
   },
 
   setTodos(punchList) {
@@ -84,7 +80,6 @@ Page({
         });
       }
     }
-    console.log(dates);
     const calendar = this.selectComponent("#calendar").calendar;
     calendar.clearTodos();
     calendar.setTodos({ dates });
@@ -111,17 +106,14 @@ Page({
         (item) => item._id === punchGoal._id
       );
       this.data.punchGoalList[index] = punchGoal;
-      const todayPunch = this.data.todayPunch.map((item) => {
-        if (item._id === punchGoal._id) {
-          return { ...item, ...punchGoal };
-        }
-        return item;
-      });
+      const todayPunch = this.data.todayPunch.map((item) =>
+        item._id === punchGoal._id ? { ...item, ...punchGoal } : item
+      );
       this.setData({ todayPunch });
     }
     if (type === 3) {
-      for (let key in this.data.punchList) {
-        for (let issue of this.data.punchList[key]) {
+      for (const key in this.data.punchList) {
+        for (const issue of this.data.punchList[key]) {
           issue.list = issue.list.filter(
             (item) => item.punchGoalId !== punchGoal._id
           );
@@ -145,80 +137,56 @@ Page({
 
   punchChange(e) {
     const { type, punch } = e;
-    const date = formatDate(punch.date);
+    const punchDate = formatDate(punch.date);
     const punchBeforeDate = formatDate(e.punchBeforeDate);
-    const year = Number(date.slice(0, 4));
-    const month = Number(date.slice(5, 7));
+    const year = Number(punchDate.slice(0, 4));
+    const month = Number(punchDate.slice(5, 7));
+    const date = Number(punchDate.slice(8, 10));
     if (type === 1 && this.data.punchList[`punchList-${year}-${month}`]) {
       this.getData(year, month);
-      if (
-        year === this.data.currentMonth.year &&
-        month === this.data.currentMonth.month &&
-        Number(date.slice(8, 10)) === this.data.selectDate.date
-      ) {
-        const { todayPunch } = this.data;
-        let punchGoal = this.data.punchGoalList.find(
-          (item) => item._id === punch.punchGoalId
-        );
-        punchGoal.comment = punch.comment;
-        todayPunch.unshift(punchGoal);
-        this.setData({ todayPunch });
+      if (this.isSelectDate(year, month, date)) {
+        this.unshiftTodayPunch(punch);
       }
     }
     if (type === 2) {
       const beforeYear = Number(punchBeforeDate.slice(0, 4));
       const beforeMonth = Number(punchBeforeDate.slice(5, 7));
-      if (
-        beforeYear === year &&
-        beforeMonth === month &&
-        this.data.punchList[`punchList-${year}-${month}`]
-      ) {
-        if (punchBeforeDate.slice(8, 10) === date.slice(8, 10)) {
-          for (let issue of this.data.punchList[`punchList-${year}-${month}`]) {
-            if (issue._id === Number(punchBeforeDate.slice(8, 10))) {
-              const index = issue.list.findIndex(
-                (item) => item._id === punch._id
-              );
-              issue.list[index] = punch;
-              break;
+      const beforeDate = Number(punchBeforeDate.slice(8, 10));
+      if (beforeYear === year && beforeMonth === month) {
+        if (this.data.punchList[`punchList-${year}-${month}`]) {
+          if (beforeDate === date) {
+            for (let issue of this.data.punchList[
+              `punchList-${year}-${month}`
+            ]) {
+              if (issue._id === date) {
+                const index = issue.list.findIndex(
+                  (item) => item._id === punch._id
+                );
+                issue.list[index] = punch;
+                break;
+              }
             }
+          } else {
+            this.splicePunchList(
+              this.data.punchList[`punchList-${year}-${month}`],
+              beforeDate,
+              punch._id,
+              true,
+              date
+            );
           }
-        } else {
-          for (let issue of this.data.punchList[`punchList-${year}-${month}`]) {
-            if (issue._id === Number(punchBeforeDate.slice(8, 10))) {
-              const index = issue.list.findIndex(
-                (item) => item._id === punch._id
-              );
-              issue.list.splice(index, 1);
-            }
-            if (issue._id === Number(date.slice(8, 10))) {
-              issue.list.push(punch);
-            }
+          if (this.isCurMonth(year, month)) {
+            this.setTodos(this.data.punchList[`punchList-${year}-${month}`]);
           }
-        }
-        if (
-          year === this.data.currentMonth.year &&
-          month === this.data.currentMonth.month
-        ) {
-          this.setTodos(this.data.punchList[`punchList-${year}-${month}`]);
         }
       } else {
         if (this.data.punchList[`punchList-${beforeYear}-${beforeMonth}`]) {
-          for (let issue of this.data.punchList[
-            `punchList-${beforeYear}-${beforeMonth}`
-          ]) {
-            const index = issue.list.findIndex(
-              (item) => item._id === punch._id
-            );
-            if (index !== -1) {
-              issue.list.splice(index, 1);
-              break;
-            }
-          }
-          if (
-            beforeYear === this.data.currentMonth.year &&
-            beforeMonth === this.data.currentMonth.month
-          ) {
+          this.splicePunchList(
+            this.data.punchList[`punchList-${beforeYear}-${beforeMonth}`],
+            beforeDate,
+            punch._id
+          );
+          if (this.isCurMonth(beforeYear, beforeMonth)) {
             this.setTodos(
               this.data.punchList[`punchList-${beforeYear}-${beforeMonth}`]
             );
@@ -227,7 +195,7 @@ Page({
         if (this.data.punchList[`punchList-${year}-${month}`]) {
           punch.punchYear = year;
           punch.punchMonth = month;
-          punch.punchDate = Number(date.slice(8, 10));
+          punch.punchDate = date;
           const index = this.data.punchList[
             `punchList-${year}-${month}`
           ].findIndex((item) => item._id === punch.punchDate);
@@ -241,63 +209,73 @@ Page({
               punch
             );
           }
-          if (
-            year === this.data.currentMonth.year &&
-            month === this.data.currentMonth.month
-          ) {
+          if (this.isCurMonth(year, month)) {
             this.setTodos(this.data.punchList[`punchList-${year}-${month}`]);
           }
         }
       }
-      if (
-        beforeYear === this.data.currentMonth.year &&
-        beforeMonth === this.data.currentMonth.month &&
-        Number(punchBeforeDate.slice(8, 10)) === this.data.selectDate.date
-      ) {
-        const { todayPunch } = this.data;
-        const index = todayPunch.findIndex((item) => item.id === punch._id);
-        todayPunch.splice(index, 1);
-        this.setData({ todayPunch });
+      if (this.isSelectDate(beforeYear, beforeMonth, beforeDate)) {
+        this.spliceTodayPunch(punch._id);
       }
-      if (
-        year === this.data.currentMonth.year &&
-        month === this.data.currentMonth.month &&
-        Number(date.slice(8, 10)) === this.data.selectDate.date
-      ) {
-        const { todayPunch } = this.data;
-        let punchGoal = this.data.punchGoalList.find(
-          (item) => item._id === punch.punchGoalId
-        );
-        punchGoal.comment = punch.comment;
-        todayPunch.unshift(punchGoal);
-        this.setData({ todayPunch });
+      if (this.isSelectDate(year, month, date)) {
+        this.unshiftTodayPunch(punch);
       }
     }
     if (type === 3 && this.data.punchList[`punchList-${year}-${month}`]) {
-      for (let issue of this.data.punchList[`punchList-${year}-${month}`]) {
-        const index = issue.list.findIndex((item) => item._id === punch._id);
-        if (index !== -1) {
-          issue.list.splice(index, 1);
-          break;
-        }
-      }
-      if (
-        year === this.data.currentMonth.year &&
-        month === this.data.currentMonth.month
-      ) {
+      this.splicePunchList(
+        this.data.punchList[`punchList-${year}-${month}`],
+        date,
+        punch._id
+      );
+      if (this.isCurMonth(year, month)) {
         this.setTodos(this.data.punchList[`punchList-${year}-${month}`]);
       }
-      if (
-        year === this.data.currentMonth.year &&
-        month === this.data.currentMonth.month &&
-        Number(date.slice(8, 10)) === this.data.selectDate.date
-      ) {
-        const { todayPunch } = this.data;
-        const index = todayPunch.findIndex((item) => item.id === punch._id);
-        todayPunch.splice(index, 1);
-        this.setData({ todayPunch });
+      if (this.isSelectDate(year, month, date)) {
+        this.spliceTodayPunch(punch._id);
       }
     }
+  },
+
+  isSelectDate(year, month, date) {
+    return (
+      year === this.data.selectDate.year &&
+      month === this.data.selectDate.month &&
+      date === this.data.selectDate.date
+    );
+  },
+  isCurMonth(year, month) {
+    return (
+      year === this.data.currentMonth.year &&
+      month === this.data.currentMonth.month
+    );
+  },
+  splicePunchList(punchList, date, id, isPush = false, pushDate) {
+    for (const issue of punchList) {
+      if (issue._id === date) {
+        const index = issue.list.findIndex((item) => item._id === id);
+        issue.list.splice(index, 1);
+        if (!isPush) break;
+      }
+      if (isPush && issue._id === pushDate) {
+        issue.list.push(punch);
+      }
+    }
+  },
+  spliceTodayPunch(id) {
+    const { todayPunch } = this.data;
+    const index = todayPunch.findIndex((item) => item.id === id);
+    todayPunch.splice(index, 1);
+    this.setData({ todayPunch });
+  },
+  unshiftTodayPunch(punch) {
+    const { todayPunch } = this.data;
+    let punchGoal = this.data.punchGoalList.find(
+      (item) => item._id === punch.punchGoalId
+    );
+    punchGoal.comment = punch.comment;
+    punchGoal.punchId = punch._id;
+    todayPunch.unshift(punchGoal);
+    this.setData({ todayPunch });
   },
 
   getGoalData() {
